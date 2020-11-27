eb61113738fd2416b289150336f7e8c4b250af30
SuperGabriel <yuandahang@outlook.com>
2020-11-27 17:05:59 +0800

    system子模块注册到eureka

--- system/src/main/java/com/gabriel/system/SystemApplication.java
+++ system/src/main/java/com/gabriel/system/SystemApplication.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 /**
  * @author Gabriel
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SystemApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(SystemApplication.class);
 
