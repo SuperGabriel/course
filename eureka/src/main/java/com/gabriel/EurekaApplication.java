2f716f0a45091658d87c827a6a8ebe49d29d57b8
SuperGabriel <yuandahang@outlook.com>
2020-11-27 16:51:41 +0800

    Eureka

--- eureka/src/main/java/com/gabriel/EurekaApplication.java
+++ eureka/src/main/java/com/gabriel/EurekaApplication.java
@@ -1,16 +1,26 @@
 package com.gabriel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
 
 /**
  * @author Gabriel
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaApplication.class);
+
+    public static void main(String[] args) {
+        Environment environment = SpringApplication.run(EurekaApplication.class, args).getEnvironment();
+        LOGGER.info("启动成功！");
+        LOGGER.info("Eureka地址：\thttp://127.0.0.1:{}", environment.getProperty("server.port"));
+    }
 
 }
