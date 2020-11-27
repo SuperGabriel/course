f7533645b31d2fe16e7a468860c3f48ca3f595a5
SuperGabriel <yuandahang@outlook.com>
2020-11-27 16:27:27 +0800

    init

---
+++ src/main/java/com/gabriel/course/CourseApplication.java
@@ -0,0 +1,16 @@
+package com.gabriel.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Gabriel
+ */
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
