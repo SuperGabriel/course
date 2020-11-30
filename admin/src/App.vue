39dea4f6036c6988a315042b5b534b2f7c39c87a
SuperGabriel <yuandahang@outlook.com>
2020-11-30 19:11:03 +0800

    admin模块

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-export default {
-  name: 'App',
-  components: {
-
-  }
-}
-</script>
-
-<style>
-</style>
